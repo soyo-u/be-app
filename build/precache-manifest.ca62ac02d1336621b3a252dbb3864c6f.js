41b2987c89f429abd26704c1b92baa1e25c8aa55
soyo <tuskshenyang@163.com>
2020-05-30 19:01:00 +0800

    add build filt to example

---
+++ build/precache-manifest.ca62ac02d1336621b3a252dbb3864c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d934de02d843dbc61cdc0cea110db521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d9ca572380dd3081c2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd9fe13908add0d508ee",
+    "url": "/static/js/2.d017d477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d017d477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d9ca572380dd3081c2",
+    "url": "/static/js/main.f595e186.chunk.js"
+  },
+  {
+    "revision": "f25555eccb8f9b6a3c74",
+    "url": "/static/js/runtime-main.b76ca4d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
